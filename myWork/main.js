c0e11a138e56f4a59dbe29fe4e88d0134294ed82
abdulaziz <119474690+javlonvic@users.noreply.github.com>
2023-01-29 10:48:15 +0500

    Add files via upload

---
+++ myWork/main.js
@@ -0,0 +1,30 @@
+//TASK 1
+// 1) let user = prompt('What is your Name ?') // ismi faqatgina a/A harfi bilan boshlanadigan odamlarni kirsin.
+// 2) let balance = prompt('How much do you have money ?') // Puli 10 000 mingdan kam bo'lmaganlarni kiriting.
+// 3) let account = prompt('Code ?') // 7777 bo'lishi kerak;
+// 4) let cash = prompt('How much do you want to cash ?') //qancha puli qoldi va qancha naqt pul oldi log da chiqarilsin;
+
+// let user = prompt('What is your name?')
+// let balance = 1000; prompt('How much do you have money?')
+// let account = 7777; prompt('Code?')
+// let cash = prompt('How much do you want to cash ?')
+
+// if (user === 'A') {
+//     console.log(user);
+
+    let user = prompt("What is your name")
+    console.log(user);
+
+    let balance = prompt("How much do you how money?")
+    console.log(balance);
+
+    let account = prompt("Code?")
+
+    let cash = prompt("How much do you mant to cash?")
+    console.log();
+
+
+    
+
+
+
